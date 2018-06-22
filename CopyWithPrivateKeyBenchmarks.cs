2172681c7690af562e0d40b64920e17419189da0
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-06-22 10:25:01 +0200

    Initial version

---
+++ CopyWithPrivateKeyBenchmarks.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace dotnet_cert_perf
+{
+    [CoreJob]
+    public class CopyWithPrivateKeyBenchmarks
+    {
+        private X509Certificate2 certificate;
+        private RSAParameters parameters;
+
+        [GlobalSetup]
+        public void Initialize()
+        {
+            this.certificate = new X509Certificate2("cert.crt");
+            this.parameters = new RSAParameters()
+            {
+                D = File.ReadAllBytes("d.bin"),
+                DP = File.ReadAllBytes("dp.bin"),
+                DQ = File.ReadAllBytes("dq.bin"),
+                Exponent = File.ReadAllBytes("exponent.bin"),
+                InverseQ = File.ReadAllBytes("inverseQ.bin"),
+                Modulus = File.ReadAllBytes("modulus.bin"),
+                P = File.ReadAllBytes("p.bin"),
+                Q = File.ReadAllBytes("q.bin"),
+            };
+        }
+
+        [Benchmark]
+        public X509Certificate2 CopyWithPrivateKey()
+        {
+            using (var rsa = RSA.Create())
+            {
+                rsa.ImportParameters(parameters);
+                return certificate.CopyWithPrivateKey(rsa);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace dotnet_cert_perf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<CopyWithPrivateKeyBenchmarks>();
+        }
+    }
+}
